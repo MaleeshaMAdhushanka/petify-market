a06425ecc0fc69ea20acdf1f7282e4c7c637a574
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-12-15 23:18:38 +0530

    check findAll and findOne in ProductService, ProductResolver with GraphQl

--- apps/pet-markt-be/src/app/app.module.ts
+++ apps/pet-markt-be/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ProductsModule } from './products/products.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: () => ({
+        autoSchemaFile: join(process.cwd(), 'apps/pet-markt-be/dist/schema.gql'),
+        sortSchema: true,
+        playground: true,
+      }),
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/pet-markt-be/src/app/products/products.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductInput } from './dto/create-product.input';
+import { UpdateProductInput } from './dto/update-product.input';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+
+  constructor (private prisma : PrismaService) {}
+  create(createProductInput: CreateProductInput) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findFirst({
+      where: {
+        id,
+      }
+    });
+  }
+
+ 
+
+  update(id: number, updateProductInput: UpdateProductInput) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
